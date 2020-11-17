5fa4b069e2827de3c742fff7e3165863b113bb3c
Raihane <raihane.akkache@gmail.com>
2020-11-17 23:47:32 +0100

    mesh generation

---
+++ Assets/Mesh Generation/Procedural Generation/Scripts/MeshGenerator.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    Vector2[] uvs;
+    Color[] colors;
+
+    public int xSize = 10;
+    public int zSize = 10;
+    public float perlinWidth = 0.3f;
+    public float perlinHeigth = 2f;
+
+    public Gradient gradient;
+
+    float maxTerrainHeight = 0;
+    float minTerrainHeight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private void Update()
+    {
+        CreateShape();
+        UpdateShape();
+    }
+
+    // Update is called once per frame
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * perlinWidth, z * perlinWidth) * perlinHeigth;
+                vertices[i] = new Vector3(x, y, z);
+                if (i == 0 || y > maxTerrainHeight)
+                    maxTerrainHeight = y;
+                if (i == 0 || y < minTerrainHeight)
+                    maxTerrainHeight = y;
+                //Debug.Log((z * (xSize + 1)) + x + ", (" + x + ", " + z + ")");
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+        int tris = 0;
+        int vert = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris] = vert;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                tris += 6;
+                vert++;
+           }
+            vert++;
+        }
+
+        uvs = new Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                uvs[i] = new Vector2((float) x / xSize, (float)z / zSize);
+                i++;
+            }
+        }
+
+        colors = new Color[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+    }
+
+    void UpdateShape()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.colors = colors;
+        
+        mesh.RecalculateNormals();
+    }
+    
+   /* private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }*/
+}
